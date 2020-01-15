7734f9fde9e1b48d06fad80871328555341182c5
Fomina Anna <50482291+KameKaizoku@users.noreply.github.com>
2020-01-15 23:33:32 +0300

    Add files via upload
    some algoritms

---
+++ Algoritm/Algoritm/MyAlgoritmas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Algoritm
+{
+    public class MyAlgoritmas
+    {
+        
+        public int GetSum(int a, int b)
+        {
+            int sum = 0;
+       
+            for(int i =Math.Min(a,b); i<=Math.Max(a,b); i++)
+                sum+=i;
+            return sum;
+        }
+        
+    }
+}
